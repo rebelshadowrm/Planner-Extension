88fa60f3c21d1517436d50aefbf3b777e02e98da
rebelshadowrm <57119466+rebelshadowrm@users.noreply.github.com>
2022-05-02 04:08:16 -0500

    Swapped to using FullCalendar Library

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
-document.addEventListener("DOMContentLoaded",  async () => {
-    const url = 'https://localhost:5001/api/EventsAPI'
-    const res = await fetch(url)
-    console.log(res)
-    if(res?.ok) {
-        const data = await res.json()
-        const cardTemplate = document.querySelector('#card-template')
-        const cardContainer = document.querySelector('.calendar-cards')
-        data.forEach( elem => {
-            const cardClone = cardTemplate.content.cloneNode(true)
-            const dateStart = cardClone.querySelector('.date-start')
-            dateStart.innerHtml = elem?.startDate
-            const dateEnd = cardClone.querySelector('.date-end')
-            dateEnd.innerHTML = elem?.endDate
-            const title = cardClone.querySelector('.title')
-            title.innerHTML = elem?.eventName
-            const description = cardClone.querySelector('.description')
-            description.innerHTML = elem?.description
-            cardContainer.append(cardClone)
-        })
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        //! set to proper port on test
+        const root = 'https://localhost:5001'
+        //! username for test
+        const username = 'rebelshadowrm@gmail.com'
+        // Api base paths
+        const eventApi = '/api/EventsAPI/'
+        const noteApi = '/api/NotesAPI/'
+
+        const calendarEl = document.querySelector('#calendar')
+        // hacky username provision, decode from JWT or get from global state
+        if(calendarEl) {
+            let calendarOptions = {
+                height: '100%',
+                slotMinTime: '08:00',
+                slotMaxTime: '20:00',
+                headerToolbar: {
+                    left: 'title',
+                    center: '',
+                    right: 'prev,next today'
+                },
+                initialView: 'listWeek',
+                initialDate: new Date().toISOString("yyyy-MM-dd"),
+                events: [],
+            }
+
+            // decode your JWT or get username from global state
+            if(username) {
+                const res = await fetch(`${root}${eventApi}${username}`)
+                if(res.status === 200) {
+                    const data = await res.json()
+                    let events = []
+                    data.forEach( e => {
+                        const event = {
+                            title: e.eventName,
+                            start: e.startDate,
+                            end: e.endDate,
+                            extendedProps: {
+                                eventId: e.eventId,
+                                description: e.description,
+                                author: e.userName
+                            }
+                        }
+                        events.push(event)
+                    })
+                    
+                    // Set events
+                    calendarOptions.events = events
+                }
+            }
+
+            const calendar = new FullCalendar.Calendar(calendarEl, calendarOptions)
+            await calendar.render()
+        }
+    } catch(err) {
+        console.log(err.message)
     }
 })
